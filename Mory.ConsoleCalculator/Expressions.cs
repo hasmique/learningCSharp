1aa3bb0dc566a6b61dbe83a4eb27aa1b6205b9a2
Vahan Mkhitaryan <vahan.mkhitaryan@betconstruct.com>
2025-04-29 11:17:03 +0400

    not good calculator

---
+++ Mory.ConsoleCalculator/Expressions.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mory.ConsoleCalculator
+{
+    interface IExpressin
+    {
+        IExpressin Calculate();
+    }
+
+    class NumberExpressin(decimal value) : IExpressin,
+        IAdditionOperators<NumberExpressin, NumberExpressin, NumberExpressin>,
+        ISubtractionOperators<NumberExpressin, NumberExpressin, NumberExpressin>,
+        IMultiplyOperators<NumberExpressin, NumberExpressin, NumberExpressin>,
+        IDivisionOperators<NumberExpressin, NumberExpressin, NumberExpressin>
+    {
+        public decimal Value { get; set; } = value;
+
+        public IExpressin Calculate() => this;
+
+        public static NumberExpressin operator +(NumberExpressin left, NumberExpressin right)
+            => new(left.Value + right.Value);
+
+        public static NumberExpressin operator -(NumberExpressin left, NumberExpressin right)
+            => new(left.Value - right.Value);
+
+        public static NumberExpressin operator *(NumberExpressin left, NumberExpressin right)
+            => new(left.Value * right.Value);
+
+        public static NumberExpressin operator /(NumberExpressin left, NumberExpressin right)
+            => new(left.Value / right.Value);
+    }
+
+    class ParenthesizedExpression : IExpressin
+    {
+        public IExpressin Inner { get; set; } = null!;
+        public IExpressin Calculate() => this.Inner.Calculate();
+    }
+
+    class AddExpressin(IExpressin left, IExpressin right) : IExpressin
+    {
+        public IExpressin Left { get; set; } = left;
+
+        public IExpressin Right { get; set; } = right;
+
+        public IExpressin Calculate() => (NumberExpressin)this.Left.Calculate() + (NumberExpressin)this.Right.Calculate();
+    }
+
+    class SubtractExpression(IExpressin left, IExpressin right) : IExpressin
+    {
+        public IExpressin Left { get; set; } = left;
+
+        public IExpressin Right { get; set; } = right;
+
+        public IExpressin Calculate() => (NumberExpressin)this.Left.Calculate() - (NumberExpressin)this.Right.Calculate();
+    }
+
+    class MultiplyExpression(IExpressin left, IExpressin right) : IExpressin
+    {
+        public IExpressin Left { get; set; } = left;
+        public IExpressin Right { get; set; } = right;
+        public IExpressin Calculate() => (NumberExpressin)this.Left.Calculate() * (NumberExpressin)this.Right.Calculate();
+    }
+
+    class DivideExpression(IExpressin left, IExpressin right) : IExpressin
+    {
+        public IExpressin Left { get; set; } = left;
+        public IExpressin Right { get; set; } = right;
+        public IExpressin Calculate() => (NumberExpressin)this.Left.Calculate() / (NumberExpressin)this.Right.Calculate();
+    }
+
+    internal class Expressions
+    {
+        static void Calc()
+        {
+            string input = "45+89";
+            int position = 0;
+
+            IExpressin read()
+            {
+                IExpressin left;
+                IExpressin right;
+                char operation = ' ';
+
+                switch (input[position])
+                {
+                    case >= '0' and <= '9':
+                        var number = String.Empty;
+
+                        for (int i = position; i < input.Length; i++)
+                        {
+                            if (input[i] is not >= '0' or not <= '9')
+                            {
+                                break;
+                            }
+
+                            number += input[i];
+                        }
+
+                        position += number.Length;
+
+                        return new NumberExpressin(decimal.Parse(number));
+                    case '+' or '-' or '*' or '/':
+                        operation = input[position];
+                        left = read();
+                        break;
+                    default:
+                        throw new NotSupportedException($"Unsupported character: {input[0]}");
+                }
+                return null;
+            }
+
+            var expression = read();
+
+            var result = (NumberExpressin)expression.Calculate();
+
+            Console.WriteLine(result.Value);
+        }
+    }
+}
---
+++ Mory.ConsoleCalculator/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mory.ConsoleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var input = Console.ReadLine();
+
+                if (String.IsNullOrEmpty(input))
+                {
+                    Console.WriteLine("Demo");
+                    input = "1+2*(6+9*2)+8";
+                }
+
+                if (input == "exit")
+                {
+                    break;
+                }
+
+                if (input == "clear")
+                {
+                    Console.Clear();
+                    continue;
+                }
+
+                input = input.Replace(" ", "");
+
+                Calculate(input);
+            }
+        }
+
+        static decimal Calculate(string input)
+        {
+            if (!input.EndsWith('='))
+            {
+                input += '=';
+            }
+
+            Console.WriteLine(input);
+
+            var expressions = new List<object>();
+
+            string temp = String.Empty;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] is >= '0' and <= '9' or '.')
+                {
+                    temp += input[i];
+                }
+                else
+                {
+                    if (temp.Length > 0)
+                    {
+                        expressions.Add(Decimal.Parse(temp));
+                        temp = String.Empty;
+                    }
+
+                    expressions.Add(input[i]);
+                }
+            }
+
+            WriteExpressions(expressions);
+
+            static decimal calcSimple(List<object> expressions)
+            {
+                Console.WriteLine("Calculating simple expression");
+
+                bool changed = true;
+
+                while (changed && expressions.Count > 1)
+                {
+                    WriteExpressions(expressions);
+
+                    changed = false;
+
+                    for (int i = 0; i < expressions.Count - 2; i++)
+                    {
+                        if (expressions[i] is decimal left && expressions[i + 1] is '*' or '/' && expressions[i + 2] is decimal right)
+                        {
+                            expressions[i] = (char)expressions[i + 1] switch
+                            {
+                                '*' => left * right,
+                                '/' => left / right,
+                                _ => throw new InvalidOperationException()
+                            };
+
+                            expressions.RemoveRange(i + 1, 2);
+
+                            changed = true;
+
+                            break;
+                        }
+                    }
+
+                    if (changed)
+                    {
+                        continue;
+                    }
+
+                    for (int i = 0; i < expressions.Count - 2; i++)
+                    {
+                        if (expressions[i] is decimal left && expressions[i + 1] is '+' or '-' && expressions[i + 2] is decimal right)
+                        {
+                            expressions[i] = (char)expressions[i + 1] switch
+                            {
+                                '+' => left + right,
+                                '-' => left - right,
+                                _ => throw new InvalidOperationException()
+                            };
+
+                            expressions.RemoveRange(i + 1, 2);
+
+                            changed = true;
+
+                            break;
+                        }
+                    }
+                }
+
+                WriteExpressions(expressions);
+
+                return (decimal)expressions[0];
+            }
+
+            var startIndex = -1;
+
+            while ((startIndex = expressions.FindLastIndex(i => i is '(')) != -1)
+            {
+                var endIndex = expressions.FindIndex(startIndex, i => i is ')');
+                var subExpressions = expressions.GetRange(startIndex + 1, endIndex - startIndex - 1);
+
+                expressions.RemoveRange(startIndex, endIndex - startIndex + 1);
+
+                var result = calcSimple(subExpressions);
+
+                expressions.Insert(startIndex, result);
+            }
+
+            return calcSimple(expressions);
+        }
+
+        private static void WriteExpressions(List<object> expressions)
+        {
+            Console.WriteLine(String.Join(' ', expressions));
+        }
+    }
+}
